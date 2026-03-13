e23b6785db7ff9a93699836279c354246a02af58
Kashi Ramanathan Valliappa <kashiramanathan2@gmail.com>
2026-03-13 22:45:39 +0530

    Made changes to requirements.txt again

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 from ultralytics import YOLO
 from PIL import Image
 
-st.title("🕸️ Spider-Verse Character Guess ft.SALVO")
+st.title("🕸Spider-Verse Character Guess ft.SALVO")
 
 model = YOLO("best.pt")
 
